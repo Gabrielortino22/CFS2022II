b3d184b00865b1e991c6d91bd3eec3074f024952
gabriel <gabrielortino@yahoo.com.ar>
2022-11-10 14:16:26 -0300

    ejercicio interfaces

---
+++ ejer1inter.js
@@ -0,0 +1,14 @@
+var autoCarrera = /** @class */ (function () {
+    function autoCarrera() {
+        this.cantidadRuedas = 4;
+        this.velocidadActual = 10;
+    }
+    return autoCarrera;
+}());
+var autoFamiliar = /** @class */ (function () {
+    function autoFamiliar() {
+        this.cantidadRuedas = 4;
+        this.velocidadActual = 10;
+    }
+    return autoFamiliar;
+}());
